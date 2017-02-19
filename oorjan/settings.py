29ce784c9d8c0d1e93e5fe9be5b262526a9f2c86
Karan Dev <karan.collegedunia@gmail.com>
2017-02-19 12:26:16 +0530

    saticfile handle

--- oorjan/settings.py
+++ oorjan/settings.py
@@ -122,7 +122,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
