a99737861b38d286f9040deecd1b741a518f077d
Karan Dev <karan.collegedunia@gmail.com>
2017-02-19 13:08:25 +0530

    saticfile admin

--- oorjan/urls.py
+++ oorjan/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # admin.autodiscover()
 
