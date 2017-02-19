9f8af030fcd84a6537b1cf1a6be1ee9e1c4c48ec
Karan Dev <karan.collegedunia@gmail.com>
2017-02-19 13:35:47 +0530

    saticfile admin

--- oorjan/settings.py
+++ oorjan/settings.py
@@ -124,6 +124,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
